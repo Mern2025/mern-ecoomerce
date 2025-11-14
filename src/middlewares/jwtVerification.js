953323896a99c0913f0947f31392834daf45f0cb
Mern2025 <merndeveloper2025@gmail.com>
2025-11-15 03:22:40 +0600

    problem

--- src/middlewares/jwtVerification.js
+++ src/middlewares/jwtVerification.js
@@ -2,15 +2,15 @@ var jwt = require('jsonwebtoken');
 const jwtverification = (req, res, next)=>{
     try{
         const token = req.headers.authorization
-
-        var decoded = jwt.verify(token, process.env.jwt_secret)
-      
-        console.log(decoded)
-
-        res.send(token)
         
+        var decoded = jwt.verify(token, process.env.jwt_secret)
+         
+        next()
+
+        res.send(decoded)
     }
     catch(err){
+        console.log('this is from catch')
        res.status(500).send(`Internal Server ${err}`)
     }
 }
