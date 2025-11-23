682c61bb78df2ffcf217964cd119b8f40e5110a4
Mern2025 <merndeveloper2025@gmail.com>
2025-11-23 23:57:58 +0600

    multer added to my category controller

--- src/routes/api/categoryApi.js
+++ src/routes/api/categoryApi.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const { add_category } = require('../../controllers/categorys')
 const categoryApi = express.Router()
+const upload = multer({dest: 'upload'})
 
-categoryApi.post('/add-category', add_category)
+categoryApi.post('/add-category', upload.single('categoryImage'), add_category)
 
 module.exports = categoryApi
 
