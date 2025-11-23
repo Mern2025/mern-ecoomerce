8240ff5bf6204a07b726df6a34dce4111193eff4
Mern2025 <merndeveloper2025@gmail.com>
2025-11-23 23:37:17 +0600

    controller catched to all data for front end

--- src/controllers/categorys.js
+++ src/controllers/categorys.js
@@ -1,5 +1,14 @@
 // ---------------add category---------------------
 
+const add_category = (req, res)=>{
+   try{
+        const {categoryName, categoryImage, creatorName, adminApproval} = req.body
+   }
+   catch(err){
+        res.status(500).send('internal server error')
+   }
+}
+
 // ---------------update category----------------
 
 
