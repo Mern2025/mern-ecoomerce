cc4305923fb4bb745b3f45f64237ecf53b2c9804
Mern2025 <merndeveloper2025@gmail.com>
2025-11-24 13:23:44 +0600

    api connect to delte category api

--- src/routes/api/categoryApi.js
+++ src/routes/api/categoryApi.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { add_category, update_category, get_all_category, get_active_category } = require('../../controllers/categorys')
+const { add_category, update_category, get_all_category, get_active_category, delete_category } = require('../../controllers/categorys')
 const multer = require('multer')
 const categoryApi = express.Router()
 const upload = multer({dest: 'upload'})
@@ -8,6 +8,7 @@ categoryApi.post('/add-category', upload.single('categoryImage'), add_category)
 categoryApi.patch('/update-category-status', update_category)
 categoryApi.get('/getAll_category', get_all_category)
 categoryApi.get('/get_active_category', get_active_category)
+categoryApi.get('/delteCategory', delete_category)
 
 module.exports = categoryApi
 
