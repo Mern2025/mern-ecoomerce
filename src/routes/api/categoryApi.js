a913fa3ca872de2f891c6e2271486f0bd9a29070
Mern2025 <merndeveloper2025@gmail.com>
2025-11-24 13:06:53 +0600

    all category exported

--- src/routes/api/categoryApi.js
+++ src/routes/api/categoryApi.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const { add_category, update_category } = require('../../controllers/categorys')
+const { add_category, update_category, get_all_category, get_active_category } = require('../../controllers/categorys')
 const multer = require('multer')
 const categoryApi = express.Router()
 const upload = multer({dest: 'upload'})
 
 categoryApi.post('/add-category', upload.single('categoryImage'), add_category)
 categoryApi.patch('/update-category-status', update_category)
+categoryApi.get('/getAll_category', get_all_category)
+categoryApi.get('/get_active_category', get_active_category)
 
 module.exports = categoryApi
 
