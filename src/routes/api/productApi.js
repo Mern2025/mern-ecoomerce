741d1cf19528f21d1608c73e2d03598b414aa045
Mern2025 <merndeveloper2025@gmail.com>
2025-11-29 02:10:24 +0600

    some problem

--- src/routes/api/productApi.js
+++ src/routes/api/productApi.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const { addProduct } = require('../../controllers/productController')
 const productApi = express.Router()
+const multer = require('multer')
+const upload = multer({des:'uploads'})
 
-productApi.post('/add-product', addProduct)
+const uploadMiddleware = upload.fields([{name:'thumbnail', maxCount:1},{name:'subImages', maxCount:8}])
+
+productApi.post('/add-product',uploadMiddleware, addProduct)
 
 
 module.exports = productApi
