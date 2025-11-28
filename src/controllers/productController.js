c1f42dda9f1186cc605f9335f747fd1f5d4945b2
Mern2025 <merndeveloper2025@gmail.com>
2025-11-29 01:09:50 +0600

    just product controler added

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,8 +1,8 @@
 const Product_Model = require("../models/Product_Model")
 const addProduct = async(req, res)=>{
     try {
-        const {tags} = req.body
-        await new Product_Model({tags}).save()
+        const {title, price, variant, categoryId, description, review, discountPrice, tags,stock} = req.body
+       
         res.send('product added')
     } catch (error) {
         console.log(error)
