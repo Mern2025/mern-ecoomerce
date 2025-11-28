9f9ba61010312f45cc94a251d9e4adbe6a11a3ee
Mern2025 <merndeveloper2025@gmail.com>
2025-11-29 01:54:11 +0600

    as a same time sku and slug generator created to contoller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,12 @@
+const { generateSKU, generateSlug } = require("../helpers/allGenerator")
 const Product_Model = require("../models/Product_Model")
+
 const addProduct = async(req, res)=>{
     try {
         const {title, price, variant, categoryId, description, review, discountPrice, tags,stock} = req.body
     //    data generator
       const slug =  generateSlug(title)
-         
+      const sku = generateSKU(title)   
         res.send('product added')
     } catch (error) {
         console.log(error)
