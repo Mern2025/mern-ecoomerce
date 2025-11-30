7fc97406e59164c3f772a97db7130c8389a80b29
Mern2025 <merndeveloper2025@gmail.com>
2025-12-01 02:33:57 +0600

    update status contoller working done

--- src/routes/api/productApi.js
+++ src/routes/api/productApi.js
@@ -1,11 +1,16 @@
 const express = require('express')
-const { addProduct } = require('../../controllers/productController')
+const { addProduct, update_Status } = require('../../controllers/productController')
 const productApi = express.Router()
 const multer  = require('multer')
+const jwtverification = require('../../middlewares/jwtVerification')
+const checkRoles = require('../../middlewares/CheckRole')
 const upload = multer({ dest: 'uploads/' })
 
 const uploadMiddleware = upload.fields([{name:'thumbnail', maxCount:1},{name:'subImages', maxCount:8}])
-productApi.post('/add-product',uploadMiddleware, addProduct)
+
+productApi.post('/add-product', jwtverification, checkRoles(['staff', 'admin']), uploadMiddleware, addProduct)
+productApi.post('/update-status', update_Status)
+
 
 
 module.exports = productApi
